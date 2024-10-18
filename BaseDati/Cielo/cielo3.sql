68908ea1fbf9571dc6507614de37b7614e7280e4
marimarafa <arafamarim166@gmail.com>
2024-10-18 19:25:23 +0200

    ..

--- BaseDati/Cielo/cielo3.sql
+++ BaseDati/Cielo/cielo3.sql
@@ -8,18 +8,34 @@ AND ap.codice = v.codice
 GROUP BY v.comp
 
 --query2
-2. Quali sono le compagnie che operano voli con durata media maggiore della durata
-media di tutti i voli?
+WITH d_voli as (SELECT codice ,sum(durataMinuti) as durata_voli
+FROM Volo 
+GROUP BY codice)
 
+SELECT v.comp , avg(v.durataMinuti)
+FROM  Volo as v , ArrPart as ap , d_voli
+WHERE ap.comp = v.comp
+AND ap.codice = v.codice 
+GROUP BY v.comp
+HAVING  avg(d_voli.durata_voli ) < avg(v.durataMinuti)
 
-
-
-
-
-
-
+--query3
 3. Quali sono le città dove il numero totale di voli in arrivo è maggiore del numero
 medio dei voli in arrivo per ogni città?
+
+WITH voli_arr as (
+    SELECT count(ap.arrivo) as num_voli
+    FROM  ArrPArt as ap
+)
+SELECT la.citta 
+FROM LuogoAeroporto as la , ArrPart as ap , voli_arr
+WHERE la.aeroporto = ap.arrivo  
+GROUP BY la.citta
+HAVING count(ap.arrivo) > avg(voli_arr.num_voli)
+
+
+
+
 4. Quali sono le compagnie aeree che hanno voli in partenza da aeroporti in Italia con
 una durata media inferiore alla durata media di tutti i voli in partenza da aeroporti
 in Italia?
