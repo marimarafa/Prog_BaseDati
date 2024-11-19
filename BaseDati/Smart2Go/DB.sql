90a6cee37ef5fca344461d7188a4624b4068574a
marimarafa <arafamarim166@gmail.com>
2024-11-19 11:41:04 +0100

    ..

---
+++ BaseDati/Smart2Go/DB.sql
@@ -0,0 +1,44 @@
+
+Utente(_cf:CodiceFiscale_, nome:varchar,cognome:varchar)
+
+Noleggio(_id:serial_, inizio:timestamp,terminato:Boolean,fine:timestamp,utente:CodiceFiscale,veicolo:varchar)
+fk:(utente) references Utente(cf);
+fk:(veicolo) references Veicolo(targa)
+
+Veicolo(_targa:varchar_)
+
+Sinistro(_id:serial_,istante:timestamp,veicolo:varchar)
+fk: (veicolo) references Veicolo(targa)
+_____________________________________________________
+
+CREATE DOMAIN CodiceFiscale as Varchar
+CHECK (value = 16);
+
+CREATE TABLE  UTENTE (
+    cf CodiceFiscale not null
+    nome varchar(100) not null
+    cognome varchar(100) not null
+    primary key(cf)
+)
+CREATE TABLE NOLEGGIO(
+    id serial not null
+    inizio timestamp not null
+    terminato Boolean not null
+    fine timestamp not null
+    utente CodiceFiscale not null
+    veicolo varchar not null
+    primary key(id)
+    foreign key(utente) references UTENTE(cf)
+    foreign key(veicolo) references VEICOLO(targa)
+)
+CREATE TABLE VEICOLO(
+    targa varchar(100) not null
+    primary key(targa)
+)
+CREATE TABLE SINISTRO(
+    id serial not null
+    istante timestamp
+    veicolo varchar
+    primary key(id)
+    foreign key(veicolo) references VEICOLO(targa)
+)
