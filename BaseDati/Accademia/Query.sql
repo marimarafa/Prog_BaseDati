4807ece49c53ea8fc437c274ea8f6603e334b0aa
marimarafa <arafamarim166@gmail.com>
2024-11-21 11:46:40 +0100

    ..

---
+++ BaseDati/Accademia/Query.sql
@@ -0,0 +1,23 @@
+--1. Quali sono le persone con stipendio di al massimo 40000 euro 
+select  *
+from persona
+where stipendio >= 40000
+
+--2ricercatori che lavorano ad almeno un progetto e hanno uno stipendio di al massimo 40000
+select p.id  , p.nome ,p.cognome
+from attivitaProgetto as ap, progetto as pr, persona as p
+where ap.progetto = pr.id
+and ap.persona = p.id 
+and p.posizione = 'Ricercatore'
+group by ap.progetto , p.id
+having count(ap.progetto) >= 1 
+
+--3.Qual è il budget totale dei progetti nel db
+select sum(budget) as budget_totale
+from progetto
+
+--4. Qual è il budget totale dei progetti a cui lavora ogni
+--persona. Per ogni persona restituire nome, cognome e
+--budget totale dei progetti nei quali è coinvolto. 
+
+
--- BaseDati/Smart2Go/DB.sql
+++ BaseDati/Smart2Go/DB.sql
@@ -1,7 +1,7 @@
 
 Utente(_cf:CodiceFiscale_, nome:varchar,cognome:varchar)
 
-Noleggio(_id:serial_, inizio:timestamp,terminato:Boolean,fine:timestamp,utente:CodiceFiscale,veicolo:varchar)
+Noleggio(_id:serial_, inizio:timestamp,terminato:Boolean,fine:timestamp*,utente:CodiceFiscale,veicolo:varchar)
 fk:(utente) references Utente(cf);
 fk:(veicolo) references Veicolo(targa)
 
@@ -24,13 +24,15 @@ CREATE TABLE NOLEGGIO(
     id serial not null
     inizio timestamp not null
     terminato Boolean not null
-    fine timestamp not null
+    fine timestamp 
     utente CodiceFiscale not null
     veicolo varchar not null
     primary key(id)
     foreign key(utente) references UTENTE(cf)
     foreign key(veicolo) references VEICOLO(targa)
-)
+    CHECK( when terminato is False then fine is null 
+    		else fine >inizio)
+    END
 CREATE TABLE VEICOLO(
     targa varchar(100) not null
     primary key(targa)
@@ -41,4 +43,5 @@ CREATE TABLE SINISTRO(
     veicolo varchar
     primary key(id)
     foreign key(veicolo) references VEICOLO(targa)
-)
+    
+REVOKE DELETE Noleggio FROM public  
