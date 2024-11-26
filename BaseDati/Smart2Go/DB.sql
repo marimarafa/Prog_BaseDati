24bc5bb4b82a512359419b0d2af39ee050d1ed53
marimarafa <arafamarim166@gmail.com>
2024-11-26 01:03:09 +0100

    ..

--- BaseDati/Smart2Go/DB.sql
+++ BaseDati/Smart2Go/DB.sql
@@ -4,6 +4,8 @@ Utente(_cf:CodiceFiscale_, nome:varchar,cognome:varchar)
 Noleggio(_id:serial_, inizio:timestamp,terminato:Boolean,fine:timestamp*,utente:CodiceFiscale,veicolo:varchar)
 fk:(utente) references Utente(cf);
 fk:(veicolo) references Veicolo(targa)
+check(when terminato = false  then fine is null
+        else fine is not null and fine > inizio)
 
 Veicolo(_targa:varchar_)
 
@@ -11,37 +13,51 @@ Sinistro(_id:serial_,istante:timestamp,veicolo:varchar)
 fk: (veicolo) references Veicolo(targa)
 _____________________________________________________
 
-CREATE DOMAIN CodiceFiscale as Varchar(16)
+BEGIN TRANSACTION;
+
+CREATE DOMAIN CodiceFiscale AS VARCHAR(16)
 CHECK (VALUE ~ '^[A-Z0-9]{16}$');
 
-CREATE TABLE  UTENTE (
-    cf CodiceFiscale not null
-    nome varchar(100) not null
-    cognome varchar(100) not null
-    primary key(cf)
-)
-CREATE TABLE NOLEGGIO(
-    id serial not null
-    inizio timestamp not null
-    terminato Boolean not null
-    fine timestamp 
-    utente CodiceFiscale not null
-    veicolo varchar not null
-    primary key(id)
-    foreign key(utente) references UTENTE(cf)
-    foreign key(veicolo) references VEICOLO(targa)
-    CHECK( when terminato is False then fine is null 
-    		else fine >inizio)
-    END
-CREATE TABLE VEICOLO(
-    targa varchar(100) not null
-    primary key(targa)
-)
-CREATE TABLE SINISTRO(
-    id serial not null
-    istante timestamp
-    veicolo varchar
-    primary key(id)
-    foreign key(veicolo) references VEICOLO(targa)
-    
-REVOKE DELETE Noleggio FROM public  
+CREATE TABLE UTENTE (
+    cf CodiceFiscale NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    cognome VARCHAR(100) NOT NULL,
+    PRIMARY KEY (cf)
+);
+
+CREATE TABLE VEICOLO (
+    targa VARCHAR(100) NOT NULL,
+    PRIMARY KEY (targa)
+);
+
+
+CREATE TABLE NOLEGGIO (
+    id SERIAL NOT NULL,
+    inizio TIMESTAMP NOT NULL,
+    terminato BOOLEAN NOT NULL,
+    fine TIMESTAMP,
+    utente CodiceFiscale NOT NULL,
+    veicolo VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (utente) REFERENCES UTENTE (cf) DEFERRABLE,
+    FOREIGN KEY (veicolo) REFERENCES VEICOLO (targa) DEFERRABLE,
+    CHECK (
+        (terminato = FALSE AND fine IS NULL) OR 
+        (terminato = TRUE AND fine IS NOT NULL AND fine > inizio)
+    )
+);
+
+
+CREATE TABLE SINISTRO (
+    id SERIAL NOT NULL,
+    istante TIMESTAMP NOT NULL,
+    veicolo VARCHAR(100),
+    PRIMARY KEY (id),
+    FOREIGN KEY (veicolo) REFERENCES VEICOLO (targa) DEFERRABLE
+);
+
+
+REVOKE DELETE ON NOLEGGIO FROM PUBLIC;
+
+COMMIT;
+ 
